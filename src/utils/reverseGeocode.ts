71f146a6572f69ef196bf31447376d84fc1c36e2
Paolo Flores <pflores.fisi22@gmail.com>
2024-03-07 15:06:33 -0500

    handle error for reverseGeocode

--- src/utils/reverseGeocode.ts
+++ src/utils/reverseGeocode.ts
@@ -2,9 +2,13 @@ const token =
   'pk.eyJ1IjoiZG90eWVpc29uIiwiYSI6ImNsdGdsbWc0OTAxbXQyanAyaGFwY20xN2MifQ.zqaP-jWUgc1FYPfcHIjtVw';
 const reverseGeocode = async ({ latitude, longitude }: { latitude: number; longitude: number }) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${token}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data.features[0].place_name;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.features[0].place_name;
+  } catch {
+    return 'Unknown address';
+  }
 };
 
 export default reverseGeocode;
