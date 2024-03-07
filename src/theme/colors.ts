72bb99a53e02ace80c24c24e7b172a455bff8fd8
Ruth Rojas <ruth.rojas4@unmsm.edu.pe>
2024-03-07 16:06:47 -0500

    colors updated

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -3,7 +3,7 @@ export const colors = {
   purple: '#010101',
   lightPurple: '#9388db',
   lightGrayPurple: '#f7f7fb',
-  pink: '#ff3d69',
+  pink: '#FF5757',
   gray: '#797777',
   black: '#010101',
   white: '#F6F8FA',
