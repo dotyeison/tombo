0e6b939f630f51b4f6ef363c1fd0a914a7e2cabc
Paolo Flores <pflores.fisi22@gmail.com>
2024-03-07 19:42:46 -0500

    add pocketbase polyfill and update dependencies

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import { Provider } from 'react-redux';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Navigator from '@navigator';
 import store from '@utils/store';
+import eventsource from 'react-native-sse';
+
+// @ts-ignore
+global.EventSource = eventsource;
 
 export default function App() {
   return (
